af10d89ffe3e3069963eb551521e830882df8c55
diegopenuela <diego.penuela@gmail.com>
2021-12-27 21:55:38 -0500

    First POST endpoint and first pydantic model

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+#Models - Pydantic
+# Defines the data schema. It does data validation
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True #Default value to True
+    rating: Optional[int] = None #Optional field. Default value to none.
 
+
+#Path Operations - FastAPI
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"message": "You are getting all the posts"}
+
+@app.post("/posts")
+async def create_posts(post: Post):
+    print(post.title)
+    print(post.dict()) #Convert pydantic model into a python dict
+    return {"data": post}
+
+#Example: Get data direclty from the body request
+#@app.post("/createposts")
+#async def create_posts(payload: dict = Body(...)):
+#    print(payload)
+#    return {"new_post": f"Title: {payload['title']}  Content: {payload['content']}"}
--- readme.md
+++ readme.md
@@ -1,7 +1,27 @@
+# VIRTUALENV
+
 virtualenv -p python3 venv
-
 source venv/bin/activate
-
 pip freeze > requirements.txt
-
 pip install -r requirements.txt
+
+
+# GITHUB 
+git config --local user.email "myemail"
+git config --local user.name "myusername"
+
+mkdir my_project
+cd my_project
+touch .gitignore
+git init
+git status
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://diegopenuela:<MYTOKEN>@github.com/diegopenuela/fastapi_social.git
+
+git remote set-url origin https://diegopenuela:<MYTOKEN>@github.com/diegopenuela/fastapi_social.git
+git push -u origin main
+
+# FASTAPI
+uvicorn main:app --reload 
